b4dae3f11f9614d8e863aab27b5525a8c808054d
D S I GUNASEKARA <148281527+shamitum@users.noreply.github.com>
2023-11-18 22:32:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,114 +8,141 @@ wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zi
 unzip ./Xray-linux-64.zip
 rm ./Xray-linux-64.zip
 
-cat > ./config.json <<EOF
-{
-    "log": {
-        "loglevel": "info"
-    },
-    "inbounds": [
-        {
-            "tag": "VMess TCP",
-            "listen": "0.0.0.0",
-            "port": 8081,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "40b1623c-6cfe-44ef-b6f6-3e9aefc1f0c7"
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "tcpSettings": {
-                    "header": {
-                        "type": "http",
-                        "request": {
-                            "method": "GET",
-                            "path": [
-                                "/"
-                            ],
-                            "headers": {
-                                "Host": [
-                                    "google.com"
-                                ]
-                            }
-                        },
-                        "response": {}
-                    }
-                },
-                "security": "none"
-            },
-            "sniffing": {
-                "enabled": true,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ]
-            }
-        },
-        {
-            "tag": "VMess Websocket",
-            "listen": "0.0.0.0",
-            "port": 8080,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "40b1623c-6cfe-44ef-b6f6-3e9aefc1f0c7"
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "/",
-                    "headers": {
-                        "Host": "google.com"
-                    }
-                },
-                "security": "none"
-            },
-            "sniffing": {
-                "enabled": true,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ]
-            }
-        }
+if test -z "$CONFIG"
+then
+    PORT=${PORT:-2083}
+    ID=${ID:-"d42e30bc-f02c-40c1-92b9-883739bf0dcf"}
+    SNI=${SNI:-"twitter.com"}
     
+    cat > ./config.json <<EOF
+{
+  "api": {
+    "services": [
+      "HandlerService",
+      "LoggerService",
+      "StatsService"
     ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "DIRECT"
+    "tag": "api"
+  },
+  "dns": null,
+  "fakeDns": null,
+  "inbounds": [
+    {
+      "listen": null,
+      "port": ${PORT},
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "email": "DSI",
+            "flow": "",
+            "id": "${ID}"
+          }
+        ],
+        "decryption": "none",
+        "fallbacks": []
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls",
+          "quic",
+          "fakedns"
+        ],
+        "enabled": true
+      },
+      "streamSettings": {
+        "grpcSettings": {
+          "multiMode": true,
+          "serviceName": ""
         },
-        {
-            "protocol": "blackhole",
-            "tag": "BLOCK"
-        }
-    ],
-    "routing": {
-        "rules": [
-            {
-                "ip": [
-                    "geoip:private"
-                ],
-                "domain": [
-                    "geosite:private"
-                ],
-                "protocol": [
-                    "bittorrent"
-                ],
-                "outboundTag": "BLOCK",
-                "type": "field"
-            }
-        ]
+        "network": "grpc",
+        "realitySettings": {
+          "dest": "${SNI}:443",
+          "maxClient": "",
+          "maxTimediff": 0,
+          "minClient": "",
+          "privateKey": "YMxPCNI7tdE_DEeQJ6zUsxqxXwrP_dKrZRiVbqCTYVY",
+          "serverNames": [
+            "${SNI}"
+          ],
+          "settings": {
+            "fingerprint": "chrome",
+            "publicKey": "7SKzmBUoSMBTbm8P1pEXnA4ERSKp6hrwLXSh2ZnH3Hc",
+            "serverName": "",
+            "spiderX": "/"
+          },
+          "shortIds": [
+            "1d78f5e7"
+          ],
+          "show": false,
+          "xver": 0
+        },
+        "security": "reality"
+      },
+      "tag": "inbound-2053"
     }
+  ],
+  "log": {
+    "error": "./error.log",
+    "loglevel": "warning"
+  },
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "0": {
+        "statsUserDownlink": true,
+        "statsUserUplink": true
+      }
+    },
+    "system": {
+      "statsInboundDownlink": true,
+      "statsInboundUplink": true
+    }
+  },
+  "reverse": null,
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "inboundTag": [
+          "api"
+        ],
+        "outboundTag": "api",
+        "type": "field"
+      },
+      {
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "blocked",
+        "type": "field"
+      },
+      {
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ],
+        "type": "field"
+      }
+    ]
+  },
+  "stats": {},
+  "transport": null
 }
-
 EOF
+else
+    echo "$CONFIG" > ./config.json
+fi
 
-./xray -c ./config.json
+ ./xray -c ./config.json
